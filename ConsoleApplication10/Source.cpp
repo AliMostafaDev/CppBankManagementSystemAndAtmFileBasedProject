c788fec5b84e0eb10d9a7fb5ed7dfe9bf439f9e1
ali <alimostafaam2004@gmail.com>
2024-05-06 14:41:13 +0300

    Add project files.

---
+++ ConsoleApplication10/Source.cpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+
+#include "clsLoginScreen.h"
+
+using namespace std;
+
+
+int main() {
+
+
+	while (true) {
+
+		if (!clsLoginScreen::ShowLoginScreen())
+			break;
+
+	}
+	
+
+
+
+	return 0;
+}
+
